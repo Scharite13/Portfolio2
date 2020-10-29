c2a739237c36c36b70605b97f42869312079318b
Derek Scharite <dms121691@gmail.com>
2020-10-29 06:07:14 -0400

    adjusted mobile view for about me section, added smooth scroll to navbar, started to adjust desktop view of project section

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react;';
+import './Contact.styles.css';
+
+class Contact extends Component{
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
