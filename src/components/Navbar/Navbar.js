75874ec433631bb716bd29fdd5805612464d51c2
Derek Scharite <dms121691@gmail.com>
2020-11-03 05:50:10 -0500

    added resume link to nav bar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Navbar.styles.css';
+import Pdf from "../../Resume/Resume.pdf";
 
 
 class NavBar extends Component {
@@ -25,7 +26,7 @@ class NavBar extends Component {
                <ul className= {this.state.clicked ? 'nav-list active' : 'nav-list'}>
                    <li className='nav-list-item'><a href="#hero">Home</a></li>
                    <li className='nav-list-item'><a href="#about-me">About me</a></li>
-                   <li className='nav-list-item'><a href="#resume">Resume</a></li>
+                   <li className='nav-list-item'><a href={Pdf} target="_black">Resume</a></li>
                    <li className='nav-list-item'><a href="#projects">Projects</a></li>
                    <li className='nav-list-item'><a href="#contact">Contact</a></li>
                </ul>
